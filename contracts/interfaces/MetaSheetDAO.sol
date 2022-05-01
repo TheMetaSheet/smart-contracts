f7497074cb03079d1ece9cfa9365de76f2148db8
Kishan Devani <kdevani964@gmail.com>
2022-05-01 22:05:42 +0400

    Created MetaSheetDAOInterface.

---
+++ contracts/interfaces/MetaSheetDAO.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./TokenInterface.sol";
+import "./NFTInterface.sol";
+
+interface MetaSheetDAOInterface {
+    function transferOwnership(address newOwner) external;
+
+    function token() external view returns (TokenInterface token);
+
+    function nft() external view returns (NFTInterface nft);
+}
