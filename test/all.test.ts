1d3a3cc2e3cf9c0e42041aa2e68f1c5a576e3802
Kishan Devani <kdevani964@gmail.com>
2022-05-01 13:05:52 +0400

    Init test for Token & NFT.

---
+++ test/all.test.ts
@@ -0,0 +1,54 @@
+import { ethers } from "hardhat";
+import { expect } from "chai";
+import { BigNumber as BigNumberJs } from "bignumber.js";
+// eslint-disable-next-line node/no-missing-import
+import { MetaSheetDAO, NFT, Token } from "../typechain";
+require("@nomiclabs/hardhat-waffle");
+
+let metaSheetDAOInstant: MetaSheetDAO = undefined as any;
+let tokenInstant: Token = undefined as any;
+let nftInstant: NFT = undefined as any;
+
+describe("Token", function () {
+  it("should deploy Token", async function () {
+    const [admin] = await ethers.getSigners();
+    const Token = await ethers.getContractFactory("Token");
+    const token = await Token.deploy(
+      "The Meta Sheet DAO Token",
+      "TMSDT",
+      admin.address,
+      new BigNumberJs(1000000000).multipliedBy(10 ** 18).toFixed()
+    );
+    tokenInstant = await token.deployed();
+    expect(tokenInstant).not.equals(undefined);
+  });
+});
+
+describe("NFT", function () {
+  it("should deploy NFt", async function () {
+    const NFT = await ethers.getContractFactory("NFT");
+    const nft = await NFT.deploy(
+      "The Meta Sheet NFT",
+      "TMSN",
+      "https://themetasheet.io"
+    );
+    nftInstant = await nft.deployed();
+    expect(nftInstant).not.equals(undefined);
+  });
+});
+
+describe("MetaSheetDAO", function () {
+  it("should deploy MetaSheetDAO", async function () {
+    const MetaSheetDAO = await ethers.getContractFactory("MetaSheetDAO");
+    const metaSheetDAO = await MetaSheetDAO.deploy();
+    metaSheetDAOInstant = await metaSheetDAO.deployed();
+    expect(metaSheetDAOInstant).not.equals(undefined);
+  });
+
+  it("set Token address", async function () {
+    await metaSheetDAOInstant.setToken(tokenInstant.address);
+    expect(await metaSheetDAOInstant.getTokenAddress()).equals(
+      tokenInstant.address
+    );
+  });
+});
