38c9ee7caf2bb3372a6a76ffa582c75a0193a64c
Pathik <pathikdevani@gmail.com>
2022-06-23 12:54:02 +0530

    fixed test case

--- test/TheMetaSheet.ts
+++ test/TheMetaSheet.ts
@@ -1,17 +1,25 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { NFT, TheMetaSheet } from "../typechain";
+
+// eslint-disable-next-line node/no-missing-import
+import { NFT, TheMetaSheet, Token } from "../typechain";
 
 describe("TheMetaSheet", function () {
-  let nft: NFT, theMetaSheet: TheMetaSheet;
+  let nft: NFT, theMetaSheet: TheMetaSheet, token: Token;
 
   this.beforeEach(async () => {
     const NFT = await ethers.getContractFactory("NFT");
     nft = await NFT.deploy();
     await nft.deployed();
 
+    const [owner] = await ethers.getSigners();
+
+    const Token = await ethers.getContractFactory("Token");
+    token = await Token.deploy("TheMetaSheet", "TMS", owner.address, 100);
+    await token.deployed();
+
     const TheMetaSheet = await ethers.getContractFactory("TheMetaSheet");
-    theMetaSheet = await TheMetaSheet.deploy(nft.address);
+    theMetaSheet = await TheMetaSheet.deploy(nft.address, token.address);
     await theMetaSheet.deployed();
   });
 
