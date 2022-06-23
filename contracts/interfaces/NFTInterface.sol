f6d05913e3180f473987fd242c37a1ca2d6114db
Pathik <pathikdevani@gmail.com>
2022-06-23 12:17:35 +0530

    removed old proejct

--- contracts/interfaces/NFTInterface.sol
+++
@@ -1,8 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-
-interface NFTInterface is IERC721 {
-    function transferOwnership(address newOwner) external;
-}
