b14ff158cc9e8ddeaf87c5317d0f4867602bbd51
Pathik <pathikdevani@gmail.com>
2022-06-23 12:11:18 +0530

    added license to each file & fixed chain-as-promised issue

--- contracts/interfaces/TheMetaSheet.sol
+++ contracts/interfaces/TheMetaSheet.sol
@@ -1,3 +1,4 @@
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -7,7 +8,7 @@ import "./NFTInterface.sol";
 interface TheMetaSheetInterface {
     function transferOwnership(address newOwner) external;
 
-    function token() external view returns (TokenInterface token);
+    function token() external view returns (TokenInterface _token);
 
-    function nft() external view returns (NFTInterface nft);
+    function nft() external view returns (NFTInterface _nft);
 }
